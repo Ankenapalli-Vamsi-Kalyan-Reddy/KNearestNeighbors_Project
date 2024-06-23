7b3d71e3c6e443c5a64677b1e5a496336d366d2e
Ankenapalli-Vamsi-Kalyan-Reddy <avamsikalyanreddy@gmail.com>
2024-06-23 11:48:13 -0400

    Implement KNN model with dataset split, initial training, evaluation, optimal K value selection using the elbow method, and re-evaluation with the chosen K value.

--- 02_k_nearest_neighbors_project.py
+++ 02_k_nearest_neighbors_project.py
@@ -54,6 +54,95 @@ scaled_features = scaler.transform(df.drop('TARGET CLASS', axis=1))
 df_feat = pd.DataFrame(scaled_features, columns = df.columns[:-1])
 df_feat.head()
 
+"""# Train Test Split
+
+**Use train_test_split to split your data into a training set and a testing set.**
+"""
+
+from sklearn.model_selection import train_test_split
+
+X = df_feat
+y = df['TARGET CLASS']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=101)
+
+"""# Using KNN
+
+**Import KNeighborsClassifier from scikit learn.**
+"""
+
+from sklearn.neighbors import KNeighborsClassifier
+
+"""**Create a KNN model instance with n_neighbors=1**"""
+
+knn = KNeighborsClassifier(n_neighbors=1)
+
+"""**Fit this KNN model to the training data.**"""
+
+knn.fit(X_train, y_train)
+
+
+
+"""# Predictions and Evaluations
+Let's evaluate our KNN model!
+
+**Use the predict method to predict values using your KNN model and X_test.**
+"""
+
+predictions = knn.predict(X_test)
+predictions
+
+"""** Create a confusion matrix and classification report.**"""
+
+from sklearn.metrics import classification_report, confusion_matrix
+
+print(confusion_matrix(y_test, predictions))
+
+
+
+print(classification_report(y_test, predictions))
+
+
+
+"""# Choosing a K Value
+Let's go ahead and use the elbow method to pick a good K Value!
+
+** Create a for loop that trains various KNN models with different k values, then keep track of the error_rate for each of these models with a list. Refer to the lecture if you are confused on this step.**
+"""
+
+delta = []
+for i in range(1, 40):
+  knn = KNeighborsClassifier(n_neighbors=i)
+  knn.fit(X_train, y_train)
+  p = knn.predict(X_test)
+  delta.append(np.mean(p != y_test))
+
+plt.figure(figsize=(11, 4))
+plt.plot(range(1, 40), delta, color='red', lw=1, linestyle='--', marker='o', markersize=4, markerfacecolor='green')
+plt.title('Error Rate vs. K Value')
+plt.xlabel('K')
+plt.ylabel('Error Rate')
+
+
+
+"""**Now create the following plot using the information from your for loop.**"""
+
+
+
+"""## Retrain with new K Value
+
+**Retrain your model with the best K value (up to you to decide what you want) and re-do the classification report and the confusion matrix.**
+"""
+
+knn = KNeighborsClassifier(n_neighbors=37)
+knn.fit(X_train, y_train)
+p = knn.predict(X_test)
+from sklearn.metrics import classification_report, confusion_matrix
+print(confusion_matrix(y_test, predictions))
+
+print(classification_report(y_test, predictions))
+
+
 
 
 
